5a1f899ebc1abf98579f1a9c847ae93cb96b4f5a
Unisa Kamara <unisk434@gmail.com>
2022-01-19 13:30:30 +0100

    Update YW50aS1wbGFnZXJpc20.js

--- assets/js/YW50aS1wbGFnZXJpc20.js
+++ assets/js/YW50aS1wbGFnZXJpc20.js
@@ -1,2 +1,1 @@
-var a=document.domain;var b=a.split(".");var c="VGhpcyBjb2RlIHdhcyB3cml0dGVuIGJ5IEpvcmdlIEEgU2FsaW5hcyBKci4gSWYgeW91IHNlZSB0aGlzIG1lc3NhZ2UsIHRoaXMgY29kZSB3YXMgbGlrZWx5IGNvcGllZC4=";var d="anNhbGluYXMyMTI="
-if(b[0]!=atob(d)&&a!=""){for(i=0;i<100;i++){alert(atob(c));}}
+
